c5f4a997fa40301e7259e6eba505ddd323c774b7
Ugbodaga Augustine <83676555+Austine4real@users.noreply.github.com>
2023-11-02 23:14:45 +0100

    General update

--- BankApp.DataAccess/Context/AppDbContext.cs
+++ BankApp.DataAccess/Context/AppDbContext.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using BankApp.Domain.Entites;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,5 +15,20 @@ namespace BankApp.DataAccess.Context
 		public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
 		{
 		}
+
+		public DbSet<Account> Accounts { get; set; }
+		public DbSet<ActivityLogEntry> ActivityLogEntries { get; set; }
+		public DbSet<Address> Addresses { get; set; }
+		public DbSet<Admin> Admins { get; set; }
+		public DbSet<BillPayment> BillPayments { get; set; }
+		public DbSet<Customer> Customers { get; set; }
+		public DbSet<Guarantor> Guarantors { get; set; }
+		public DbSet<Loan> Loans { get; set; }
+		public DbSet<NextOfKin> NextOfKins { get; set; }
+		public DbSet<Notification> Notifications { get; set; }
+		public DbSet<PhoneNumber> PhoneNumbers { get; set; }
+		public DbSet<Staff> Staff { get; set; }
+		public DbSet<Transactions> Transactions { get; set; }
+		public DbSet<User> Users { get; set; }
 	}
 }
