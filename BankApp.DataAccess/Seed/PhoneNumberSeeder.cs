2d54ef200547c6de2f9239c8967cecb6d7d3119a
Henry Uzor <henryuzo0@gmail.com>
2023-11-10 10:17:53 +0100

    Seeder completed

---
+++ BankApp.DataAccess/Seed/PhoneNumberSeeder.cs
@@ -0,0 +1,48 @@
+﻿using BankApp.Domain.Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankApp.DataAccess.Seed
+{
+    internal class PhoneNumberSeeder
+    {
+        private readonly AppDbContext _context;
+
+        public PhoneNumberSeeder(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public void SeedData()
+        {
+            if (!_context.PhoneNumbers.Any())
+            {
+                // Add initial phone number data
+                var phoneNumbers = new[]
+                {
+                new PhoneNumber
+                {
+                    Id = Guid.NewGuid(),
+                    Extension = "+1",
+                    Number = "555-123-4567",
+                    UserId = Guid.NewGuid()
+                },
+                new PhoneNumber
+                {
+                    Id = Guid.NewGuid(),
+                    Extension = "+44",
+                    Number = "20-1234-5678",
+                    UserId = Guid.NewGuid()
+                },
+                // Add more phone number entries as needed
+            };
+
+                _context.PhoneNumbers.AddRange(phoneNumbers);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
