5d61690659fb97615efae5310579811ab8e31c27
Ugbodaga Augustine <83676555+Austine4real@users.noreply.github.com>
2023-11-15 13:06:57 +0100

    update on user-cration

--- BankApp.DTOs/RequestDTOs/AddressRequestDTO.cs
+++ BankApp.DTOs/RequestDTOs/AddressRequestDTO.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Text.Json.Serialization;
@@ -9,12 +10,21 @@ namespace BankApp.DTOs.RequestDTOs
 {
 	public class AddressRequestDTO
 	{
+		[StringLength(5, ErrorMessage = "Postal code cannot exceed 5 characters.")]
 		public string PostalCode { get; set; } = string.Empty;
+
+		[Required(ErrorMessage = "Street is required.")]
 		public string Street { get; set; } = string.Empty;
+
+		[Required(ErrorMessage = "City is required.")]
 		public string City { get; set; } = string.Empty;
+
 		public string State { get; set; } = string.Empty;
+
+		[Required(ErrorMessage = "Country is required.")]
 		public string Country { get; set; } = string.Empty;
+
 		[JsonIgnore]
-		public Guid UserId { get; set; } 
+		public Guid UserId { get; set; }
 	}
 }
